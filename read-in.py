16dbad17fe10e2436423f566719b6aff3cf53744
Elan S Parsons <elansparsons@gmail.com>
2018-10-12 13:51:49 -0400

    begin reading in, cleaning datasets

---
+++ read-in.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+##TODO: create function to clean data files
+
+
+##coverted txt to csv, read in
+cite_1 = pd.read_csv("./data/2017-01to06-citeSkrutskie-NOTES.csv", skiprows=14, sep=',', header=None)
+cite_1.columns = ["article", "note1", "note2", "note3", "note4", "note5"]
+cite_1 = cite_1.iloc[:, 0:2]
+
+##remove ambiguous answers
+notna = cite_1['note1'].str.contains("YES|NO")
+cite_1 = cite_1[notna]
+##create col of only yes/no
+cite_1['cited'] = np.where(cite_1['note1'].str.contains("YES"), 'yes', 'no')
+
+##fetch articles from API
+
+##establish model (word2vec for topic identification?)
