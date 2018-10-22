f84468e1345d022f46ef7a7a74673b1fb61b832b
Elan S Parsons <elansparsons@gmail.com>
2018-10-21 20:18:09 -0400

    fixed ADS API pull

--- read-in.py
+++ read-in.py
@@ -17,23 +17,39 @@ cite_1 = cite_1[notna]
 ##create col of only yes/no
 cite_1['cited'] = np.where(cite_1['note1'].str.contains("YES"), 'yes', 'no')
 
-##isolate article id to bibcode
+##isolate article id to bibcode, reset index to prevent key errors
 cite_1['bibcode'] = cite_1['article'].str.slice(start=9)
-cite_1.set_index('article')
+cite_1 = cite_1.reset_index()
 
 ##fetch article arXiv ID from API - search by bibcode
 token = 'Fv6aJu1i3oV4uuG6LfStmnIg9Txxe1NfFY2vLRap'
 
 doi = []
 
-for bibcode in range(100):
+for bibcode in range(len(cite_1['bibcode'])-1):
 
     r = requests.get("https://api.adsabs.harvard.edu/v1/search/query?q=bibcode:" + cite_1['bibcode'][bibcode] + "&fl=doi", headers = {"Authorization": "Bearer " + token})
     newtext = json.loads(r.text)
-    if not newtext['response']['docs'][0]:
+    if not newtext['response']['docs']:
         doi.append(np.nan)
     else:
-        doi.append(newtext['response']['docs'][0]['doi'])
+        if not newtext['response']['docs'][0]:
+            doi.append(np.nan)
+        else:
+            doi.append(newtext['response']['docs'][0]['doi'])
 
+
+#for bibcode in range(42):
+
+    r = requests.get("https://api.adsabs.harvard.edu/v1/search/query?q=bibcode:" + cite_1['bibcode'][161] + "&fl=doi",
+                 headers = {"Authorization": "Bearer " + token})
+    newtext = json.loads(r.text)
+    if not newtext['response']['docs']:
+        doi.append(np.nan)
+    else:
+        if not newtext['response']['docs'][0]:
+            doi.append(np.nan)
+        else:
+            doi.append(newtext['response']['docs'][0]['doi'])
 
 ##fetch body text from arXiv
