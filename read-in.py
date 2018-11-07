c7f69c8d783d94f01dcefe3f3fe9829666e2f216
Elan S Parsons <elansparsons@gmail.com>
2018-11-06 23:47:17 -0500

    built function to grab most PDFs if available

--- read-in.py
+++ read-in.py
@@ -21,12 +21,12 @@ cite_1['cited'] = np.where(cite_1['note1'].str.contains("YES"), 'yes', 'no')
 cite_1['bibcode'] = cite_1['article'].str.slice(start=9)
 cite_1 = cite_1.reset_index()
 
-##fetch article arXiv ID from API - search by bibcode
+##fetch article doi from API - search by bibcode
 token = 'Fv6aJu1i3oV4uuG6LfStmnIg9Txxe1NfFY2vLRap'
 
 doi = []
 
-for bibcode in range(len(cite_1['bibcode'])-1):
+for bibcode in range(len(cite_1['bibcode'])):
 
     r = requests.get("https://api.adsabs.harvard.edu/v1/search/query?q=bibcode:" + cite_1['bibcode'][bibcode] + "&fl=doi", headers = {"Authorization": "Bearer " + token})
     newtext = json.loads(r.text)
@@ -39,17 +39,56 @@ for bibcode in range(len(cite_1['bibcode'])-1):
             doi.append(newtext['response']['docs'][0]['doi'])
 
 
-#for bibcode in range(42):
+doi = pd.Series(doi)
+pattern = '\[|\]'
+cite_1['doi'] = doi.str[0]
 
-    r = requests.get("https://api.adsabs.harvard.edu/v1/search/query?q=bibcode:" + cite_1['bibcode'][161] + "&fl=doi",
-                 headers = {"Authorization": "Bearer " + token})
-    newtext = json.loads(r.text)
-    if not newtext['response']['docs']:
-        doi.append(np.nan)
-    else:
-        if not newtext['response']['docs'][0]:
-            doi.append(np.nan)
-        else:
-            doi.append(newtext['response']['docs'][0]['doi'])
+# export csv of identifiers
+cleandf = cite_1.drop(['index','note1'],axis=1)
+cleandf.to_csv("cleandf.csv",sep=',')
 
-##fetch body text from arXiv
+# find and download arxiv PDF with DOI
+
+import urllib
+
+#function to grab pdfs and store locally
+def getthempdfs(doi_ids):
+    retrieve = []
+    for linenum, line in enumerate(doi_ids):
+        if line != '':
+            url = 'http://export.arxiv.org/api/query?search_query=all:' + line + '&start=0&max_results=1'
+            data = urllib.request.urlopen(url).read()
+            urlbegin = data.decode('utf8').find('http://arxiv.org/pdf/')  # beginning of pdf string
+            if urlbegin != -1:
+                if data[urlbegin] != 34:
+                    pdfurl = data[urlbegin:urlbegin + 30]
+                    urllib.request.urlretrieve(pdfurl.decode('utf8'), str(linenum) + '.pdf')
+                else:
+                    pdfurl = data[urlbegin + 1:urlbegin +31]
+                    urllib.request.urlretrieve(pdfurl.decode('utf8'), str(linenum) + '.pdf')
+            else: retrieve.append(linenum)
+        else: continue
+    return retrieve
+
+getthempdfs(bits)
+
+bits = cleandf["doi"][:5] # index 1 & 4 are problem
+
+doi = '10.2298/SAJ160802003A'
+url = 'http://export.arxiv.org/api/query?search_query=all:'+ doi + '&start=0&max_results=1'
+data = urllib.request.urlopen(url).read()
+urlbegin = data.decode('utf8').find('http://arxiv.org/pdf/') # beginning of pdf string
+pdfurl = data[urlbegin + 1:urlbegin +31]
+pdfurl = data[urlbegin:urlbegin+30]
+
+urllib.request.urlretrieve(pdfurl.decode('utf8'),'thatwhat.pdf')
+
+
+#extract and read PDF
+from urllib.request import urlretrieve
+import PyPDF2
+
+urlretrieve('https://arxiv.org/pdf/1705.09063.pdf','whatwhat.pdf')
+here = PyPDF2.PdfFileReader(open('whatwhat.pdf', 'rb'))
+pg = here.getPage(0)
+texts = pg.extractText()
