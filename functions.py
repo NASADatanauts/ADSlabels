132b603e81f156e0c7a46ef92b2eb2b272e1f1f1
Elan S Parsons <elansparsons@gmail.com>
2018-12-03 20:19:32 -0500

    separated functions out into functions.py

---
+++ functions.py
@@ -0,0 +1,70 @@
+import urllib
+from bs4 import BeautifulSoup
+
+#function to grab pdfs and store locally - takes a list of DOIs, returns line numbers if not on arxiv and NA when no PDF found
+def getthempdfs(doi_ids):
+    retrieve = []
+    for linenum, line in enumerate(doi_ids):
+        if line != '' and not pd.isnull(line):
+            url = 'http://export.arxiv.org/api/query?search_query=all:' + line + '&start=0&max_results=1'
+            data = urllib.request.urlopen(url).read()
+            urlbegin = data.decode('utf8').find('http://arxiv.org/pdf/')  # beginning of pdf string
+            if urlbegin != -1:
+                soup = BeautifulSoup(data, 'html.parser')
+                tags = soup.find_all('link')
+                for tag in tags:
+                    maypdf = tag.get('href')
+                    if maypdf.find('arxiv.org/pdf') != -1:
+                        urllib.request.urlretrieve(maypdf, './pdfs/' + str(linenum) + '.pdf')
+                    else: retrieve.append("NA")
+            else: retrieve.append(linenum)
+        else: continue
+    return retrieve
+
+
+
+#extract and read PDF
+from urllib.request import urlretrieve
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import TextConverter
+from io import StringIO
+from pdfminer.layout import LAParams
+import os
+
+# takes filepath, returns text of single PDF document
+def pdf_to_text(path):
+    here = open(path, 'rb')
+    retstr = StringIO()
+    laparams = LAParams()
+
+    mgr = PDFResourceManager()
+    device = TextConverter(mgr, retstr, codec='utf-8', laparams=laparams)
+    interpret = PDFPageInterpreter(mgr, device)
+
+    for page in PDFPage.get_pages(here, check_extractable=True):
+        interpret.process_page(page)
+
+    text = retstr.getvalue()
+
+    here.close()
+    device.close()
+    retstr.close()
+
+    return text
+
+
+# takes open directory, save directory -- saves .txt from PDF in open directory to save directory
+def pdf_text_save(open_dir, save_dir):
+    filelist = os.listdir(open_dir)
+    for file in filelist:
+        text = pdf_to_text(open_dir + '/' + file)
+        lowertext = text.lower()
+        begin = lowertext.find("introduction")
+        end = lowertext.rfind("references")
+        prune_text = lowertext[begin:end]
+
+        f = open(save_dir + '/' + str(file) + '.txt', 'w', encoding='utf-8')
+        f.write(prune_text)
+        f.close()
--- read-in.py
+++ read-in.py
@@ -48,84 +48,13 @@ cleandf = cite_1.drop(['index','note1'],axis=1)
 cleandf.to_csv("cleandf.csv",sep=',')
 
 # find and download arxiv PDF with DOI
-
-import urllib
-from bs4 import BeautifulSoup
-import lxml
-
-#function to grab pdfs and store locally - takes a list of DOIs, returns indexes that fail
-def getthempdfs(doi_ids):
-    retrieve = []
-    for linenum, line in enumerate(doi_ids):
-        if line != '' and not pd.isnull(line):
-            url = 'http://export.arxiv.org/api/query?search_query=all:' + line + '&start=0&max_results=1'
-            data = urllib.request.urlopen(url).read()
-            urlbegin = data.decode('utf8').find('http://arxiv.org/pdf/')  # beginning of pdf string
-            if urlbegin != -1:
-                soup = BeautifulSoup(data, 'html.parser')
-                tags = soup.find_all('link')
-                for tag in tags:
-                    if tag.get('href').find('pdf'):
-                        urllib.request.urlretrieve(pdfurl.decode('utf8'), './pdfs/' + str(linenum) + '.pdf')
-                    else: retrieve.append("NA")
-            else: retrieve.append(linenum)
-        else: continue
-    return retrieve
-
-getthempdfs(bits)
 print(getthempdfs(cleandf["doi"]))
-getthempdfs(cleandf['doi'][40:50])
+#returns 115 out of 312 in original batch
 
-
-#extract and read PDF
-from urllib.request import urlretrieve
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.pdfpage import PDFPage
-from pdfminer.converter import TextConverter
-from io import StringIO
-from pdfminer.layout import LAParams
-import os
 
 urlretrieve('https://arxiv.org/pdf/1705.09063.pdf','./pdfs/whatwhat.pdf')
 
-# takes filepath, returns text of single document
-def pdf_to_text(path):
-    here = open(path, 'rb')
-    retstr = StringIO()
-    laparams = LAParams()
-
-    mgr = PDFResourceManager()
-    device = TextConverter(mgr, retstr, codec='utf-8', laparams=laparams)
-    interpret = PDFPageInterpreter(mgr, device)
-
-    for page in PDFPage.get_pages(here, check_extractable=True):
-        interpret.process_page(page)
-
-    text = retstr.getvalue()
-
-    here.close()
-    device.close()
-    retstr.close()
-
-    return text
-
-
 fulltext = pdf_to_text('whatwhat.pdf')
 
-# take open directory, save directory -- saves .txt from PDF to save directory
-def pdf_text_save(open_dir, save_dir):
-    filelist = os.listdir(open_dir)
-    for index, file in enumerate(filelist):
-        text = pdf_to_text(open_dir + '/' + file)
-        lowertext = text.lower()
-        begin = lowertext.find("introduction")
-        end = lowertext.rfind("references")
-        prune_text = lowertext[begin:end]
-
-        f = open(save_dir + '/' + str(index) + '.txt', 'w', encoding='utf-8')
-        f.write(prune_text)
-        f.close()
-
-
+# take pdfs from first directory, convert to text, save in second directory
 pdf_text_save('./pdfs','./txts')
